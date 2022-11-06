2aff4f3c2a58cb1ee4a995ce560cab8a01618efe
ThaProphet <85190673+ThaProphet15@users.noreply.github.com>
2022-11-06 11:07:41 -0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# TO USE: Set which functions you want to run to true
+
 doFirewall=false
+doSSH=false
+doSystemCtl=false
 doUsers=false # Work in progress
 
 
@@ -13,7 +17,18 @@ function enableFirewall { # Doesn't fully secure ports or deny ping requests
 
 function enableSSH {
     apt install -y openssh-server
+    ufw allow ssh
+}
 
+function handleSystemCtl {
+    sysctl -w net.ipv4.tcp_syncookies=1
+	sysctl -w net.ipv4.ip_forward=0
+	sysctl -w net.ipv4.conf.all.send_redirects=0
+	sysctl -w net.ipv4.conf.default.send_redirects=0
+	sysctl -w net.ipv4.conf.all.accept_redirects=0
+	sysctl -w net.ipv4.conf.default.accept_redirects=0
+	sysctl -w net.ipv4.conf.all.secure_redirects=0
+	sysctl -w net.ipv4.conf.default.secure_redirects=0
 }
 
 function handleUsers { # Work in progress
@@ -21,3 +36,20 @@ function handleUsers { # Work in progress
     regulars=()
     exemptions=()
 }
+
+
+if [ $doFirewall = true ]; then
+    enableFirewall
+fi
+
+if [ $doSSH = true ]; then
+    enableSSH
+fi
+
+if [ $doUsers = true ]; then
+    handleUsers
+fi
+
+if [ $doSystemCtl = true ]; then
+    handleSystemCtl
+fi
